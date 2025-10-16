0d1ea600ebc50c7512fae05394515fb1057a59e2
muhammad-tamim <contact2tamim@gmail.com>
2025-10-16 14:55:20 +0600

    feat(src/shared/components/structure/): add hero

--- src/shared/components/structure/Hero.jsx
+++ src/shared/components/structure/Hero.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import Button from '../ui/Button';
+import bannerImg from '../../../assets/images/banner.png'
 
 const Hero = () => {
     return (
-        <div>
-            Hero
+        <div className='py-12'>
+            <img
+                src={bannerImg}
+                alt='Banner Image'
+                className='w-full mx-auto md:max-w-md'
+            />
+            <div className='text-center space-y-4'>
+                <h1 className='font-thin text-7xl text-gray-900'>
+                    Browse, Search, View, Buy
+                </h1>
+                <p className='text-gray-500'>
+                    Best place to browse, search, view details and purchase of top
+                    flagship phones <br /> of the current time - FlagshipFaceOff
+                </p>
+                <form
+                    className='flex flex-col md:flex-row justify-center items-center mb-4 md:px-24'
+                >
+                    <input
+                        type='text'
+                        placeholder='Search Phone by Name'
+                        className='bg-white border border-gray-300 rounded shadow-md w-2/3 h-12 px-4 mb-3
+            focus:outline-none focus:shadow-outline md:mr-2 md:mb-0'
+                    />
+                    <Button type="submit" label="Search"></Button>
+                </form>
+            </div>
         </div>
     );
 };
