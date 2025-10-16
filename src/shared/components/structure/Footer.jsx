488d73f54d8f796e35916b2e1c2917117bbebc61
muhammad-tamim <contact2tamim@gmail.com>
2025-10-16 14:53:35 +0600

    feat(src/shared/components/structure): add footer

--- src/shared/components/structure/Footer.jsx
+++ src/shared/components/structure/Footer.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className='footer sm:footer-horizontal footer-center bg-base-200 text-base-content p-4'>
+            <aside>
+                <p>
+                    Copyright © {new Date().getFullYear()} - All right reserved by
+                    FlagshipFaceOff Ltd
+                </p>
+            </aside>
+        </footer>
     );
 };
 
